4622aae20afb11042f2de8e77529b3fece1f74d2
rosamondkim <96304623+rosamondkim@users.noreply.github.com>
2022-07-08 01:25:26 +0900

    Add files via upload

--- challenge1.js
+++ challenge1.js
@@ -1,14 +1,30 @@
-const title = document.getElementById("title");
+// import "./styles.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+// <⚠️ /DONT DELETE THIS ⚠️>
 
-function handleClickTitle(){
-    const currentColor = title.style.color;
-    let newColor ;
-    if (currentColor === "black"){
-        newColor = "green";
-    } else {
-        newColor = "black";
+
+const title = document.querySelector("h2");
+
+const superEventHandler = {
+    mouseenter:function(){
+        title.style.color= colors[0];
+        title.innerText ="The mouse is here!";
+    },
+    mouseleave:function(){
+        title.style.color= colors[1];
+        title.innerText = "The mouse is gone!";
+    },
+    contextmenu:function(){
+        title.style.color= colors[4];
+        title.innerText = "That was a right click!";
+    },
+    resize:function(){
+        title.style.color= colors[2];
+        title.innerText = "You just resized!";
     }
-    title.style.color = newColor;
-}
-
-title.addEventListener("click",handleClickTitle);
+};
+    
+title.addEventListener("mouseenter",superEventHandler.mouseenter);
+title.addEventListener("mouseleave",superEventHandler.mouseleave);
+window.addEventListener("contextmenu",superEventHandler.contextmenu);
+window.addEventListener("resize",superEventHandler.resize);
